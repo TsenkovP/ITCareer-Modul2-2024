581eebe835b040c1a347cebe5917c9b83d58c228
TsenkovP <133803692+TsenkovP@users.noreply.github.com>
2024-02-01 18:42:01 +0200

    Add files via upload

---
+++ 01.ArrayReverse/01.ArrayReverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ArrayReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            int[] numbersReversed = numbers
+                .Reverse()
+                .ToArray();
+            for (int i = 0; i < numbersReversed.Length; i++)
+            {
+                Console.Write($"{numbersReversed[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
