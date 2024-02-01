935caa0b150e2d651b809b90c0c05b50f1e8a7e5
TsenkovP <133803692+TsenkovP@users.noreply.github.com>
2024-02-01 19:42:57 +0200

    Add files via upload

---
+++ 02.StringArrayReverse/02.StringArrayReverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.StringArrayReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string[] reversedInput = input.Reverse().ToArray();
+            foreach (var item in reversedInput)
+            {
+                Console.Write($"{item} ");  
+            }
+            
+
+                
+        }
+    }
+}
